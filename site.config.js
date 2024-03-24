bae5b7d98ddd3441021569a340252bf95345c056
cheongsando <88742184+cheongsando@users.noreply.github.com>
2024-03-25 00:46:59 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,31 +1,35 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "KIM, CHEONGSAN",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Cloud Engineer | BE",
+    bio: "developer onion koongya",
+    email: "desertisland@outlook.kr",
+    linkedin: "cheongsando",
+    github: "cheongsando",
+    instagram: "cheongsando_",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `VEGECRAFT`,
+      href: "https://proj.cheongsan.do/vegecraft",
+    },
+    {
+      name: `KOONGRAIL`,
+      href: "https://proj.cheongsan.do/koongrail",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "CHEONGSANDO",
+    description: "desertisland",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://cheongsando.vercel.app",
+  since: 2024, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
