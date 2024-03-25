0d278e2ae7ad555dd4fec60056dc0b7c43dfa39b
CHEONGSANDO <cheongsando@hotmail.com>
2024-03-26 04:50:02 +0900

    웹 페이지에 폰트 적용

--- src/layouts/RootLayout/ThemeProvider/Global/index.tsx
+++ src/layouts/RootLayout/ThemeProvider/Global/index.tsx
@@ -1,7 +1,9 @@
 import { Global as _Global, css, useTheme } from "@emotion/react"
 
 import { ThemeProvider as _ThemeProvider } from "@emotion/react"
-import { pretendard } from "src/assets"
+import { red } from "@radix-ui/colors"
+import { notosans } from "src/assets/fonts/notosanskorean"
+import { redhatdisplay } from "src/assets/fonts/redhat"
 
 export const Global = () => {
   const theme = useTheme()
@@ -14,9 +16,9 @@ export const Global = () => {
           padding: 0;
           color: ${theme.colors.gray12};
           background-color: ${theme.colors.gray2};
-          font-family: ${pretendard.style.fontFamily};
-          font-weight: ${pretendard.style.fontWeight};
-          font-style: ${pretendard.style.fontStyle};
+          font-family: ${redhatdisplay.style.fontFamily}, ${notosans.style.fontFamily};
+          font-weight: ${redhatdisplay.style.fontWeight}, ${notosans.style.fontWeight};
+          font-style: ${redhatdisplay.style.fontStyle}, ${notosans.style.fontStyle};
         }
 
         * {
