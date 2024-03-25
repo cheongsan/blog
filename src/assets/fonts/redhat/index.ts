46b25793c2087c356e299c64d236e97beea9ca5e
CHEONGSANDO <cheongsando@hotmail.com>
2024-03-26 04:38:14 +0900

    폰트 수정

---
+++ src/assets/fonts/redhat/index.ts
@@ -0,0 +1,7 @@
+import { Red_Hat_Display } from 'next/font/google'
+
+export const redhatdisplay = Red_Hat_Display({
+  weight: ['300', '500', '700', '900'],
+  style: ['normal', 'italic'],
+  subsets: ['latin-ext'],
+})
