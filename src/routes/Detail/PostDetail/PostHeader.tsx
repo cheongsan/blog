c7f1b0d4b1540c98894454db1a9d2ab8fdb03f40
Cheongsan Kim <land@cheongsan.do>
2024-03-27 11:49:04 +0900

    Gravatar 프로필 이미지로 표시되도록 오류 수정

--- src/routes/Detail/PostDetail/PostHeader.tsx
+++ src/routes/Detail/PostDetail/PostHeader.tsx
@@ -3,6 +3,7 @@ import Tag from "src/components/Tag"
 import { TPost } from "src/types"
 import { formatDate } from "src/libs/utils"
 import Image from "next/image"
+import Gravatar from 'react-gravatar';
 import React from "react"
 import styled from "@emotion/styled"
 
@@ -20,13 +21,7 @@ const PostHeader: React.FC<Props> = ({ data }) => {
             {data.author && data.author[0] && data.author[0].name && (
               <>
                 <div className="author">
-                  <Image
-                    css={{ borderRadius: "50%" }}
-                    src={data.author[0].profile_photo || CONFIG.profile.image}
-                    alt="profile_photo"
-                    width={24}
-                    height={24}
-                  />
+                  <Gravatar email={CONFIG.profile.email} size={24} rating="pg" default="identicon" />
                   <div className="">{data.author[0].name}</div>
                 </div>
                 <div className="hr"></div>
@@ -84,6 +79,9 @@ const StyledWrapper = styled.div`
         display: flex;
         gap: 0.5rem;
         align-items: center;
+        .react-gravatar {
+          border-radius: 1rem;
+        }
       }
       .hr {
         margin-top: 0.25rem;
