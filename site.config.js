ec3bb436ee04fc512f68532a847d0d519e282c80
CHEONGSANDO(RYAN's Mac) <cheongsando@hotmail.com>
2024-06-26 07:01:03 +0900

    시스템 테마를 사용하도록 수정

--- site.config.js
+++ site.config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   blog: {
     title: "CHEONGSANDO",
     description: "desertisland",
-    scheme: "dark", // 'light' | 'dark' | 'system'
+    scheme: "system", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
