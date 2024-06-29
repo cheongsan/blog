21f4d8395bc2f0f2e6fef9036ecbcb09976ffa34
Cheongsan Kim <land@cheongsan.do>
2024-06-30 03:34:29 +0900

    피드 화면의 카테고리 선택 타이틀에서 카테고리의 아이콘의 사이즈 조정

--- src/components/Category/IconRenderer.tsx
+++ src/components/Category/IconRenderer.tsx
@@ -1,13 +1,18 @@
 import React from "react"
 import { emojiIconMap } from "./emojiIconMap"
 
-const IconRenderer: React.FC<{ category: string }> = ({ category }) => {
+interface IconRendererProps {
+  category: string
+  size?: number
+}
+
+const IconRenderer: React.FC<IconRendererProps> = ({ category, size = 20 }) => {
   const [emoji, ...textParts] = category.split(" ")
   const text = textParts.join(" ")
   const IconComponent = emojiIconMap[emoji] || null
   return (
     <>
-      {IconComponent ? React.createElement(IconComponent, { size: "20" }) : emoji} {text}
+      {IconComponent ? React.createElement(IconComponent, { size }) : emoji} {text}
     </>
   )
 }
