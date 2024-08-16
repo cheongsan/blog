0a119eddf3db3525d3da45244318572cdf020dfa
CHEONGSANDO(RYAN's PC) <cheongsando@hotmail.com>
2024-08-16 11:51:27 +0900

    shadcn/ui 추가

--- src/libs/utils/index.ts
+++ src/libs/utils/index.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 export function formatDate(date: any, local: any) {
   const d = new Date(date)
   const options: any = { year: 'numeric', month: 'short', day: 'numeric' }
