747a36e0d8a95e5d84f2c34b23b574f927b4d424
Cheongsan Kim <land@cheongsan.do>
2024-08-30 11:26:52 +0900

    다크 모드 CSS 파일 불러오는 방식 수정 : 오리지널에서는 main에 dark-mode 또는 dark-theme 클래스로 불러옴. 이와 유사한 방식으로 다크 모드 클래스가 추가되도록 수정함

--- components/layout/ThemeProvider.tsx
+++ components/layout/ThemeProvider.tsx
@@ -1,9 +1,29 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    return (
+        <NextThemesProvider {...props}>
+            <ThemeWrapper>{children}</ThemeWrapper>
+        </NextThemesProvider>
+    )
+}
+
+function ThemeWrapper({ children }: { children: React.ReactNode }) {
+    const { resolvedTheme } = useTheme()
+
+    React.useEffect(() => {
+        const htmlElement = document.documentElement
+
+        if (resolvedTheme === "dark") {
+            htmlElement.classList.add("dark-theme", "dark-mode")
+        } else {
+            htmlElement.classList.remove("dark-theme", "dark-mode")
+        }
+    }, [resolvedTheme])
+
+    return <>{children}</>
 }
