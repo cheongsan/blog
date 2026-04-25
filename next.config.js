fe5991280dcebdb73ef96af454061c67bbafaf09
Cheongsan Kim <developer@cheongsan.kim>
2026-04-26 04:54:05 +0900

    remove: delete all FastAPI code, move contributions to Next.js API Route

--- next.config.js
+++ next.config.js
@@ -2,9 +2,4 @@ module.exports = {
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com', 'prod-files-secure.s3.us-west-2.amazonaws.com'],
   },
-  async rewrites() {
-    return process.env.NODE_ENV === 'development'
-      ? [{ source: '/py-api/:path*', destination: 'http://localhost:8000/py-api/:path*' }]
-      : []
-  },
 }
